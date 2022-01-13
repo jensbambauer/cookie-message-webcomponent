55b4f4501e3c152ce166230ebd6805fa523e8292
Jens Bambauer <jens.bambauer@gmx.de>
2022-01-13 20:15:25 +0100

    new tag manager invoke

--- src/cookie-message.ts
+++ src/cookie-message.ts
@@ -66,12 +66,19 @@ export class CookieMessage extends LitElement {
 
   private _initGoogleTag() {
     if (Cookies.get('cookie_consent_tracking')) {
-      window.dataLayer = window.dataLayer || [];
-      function gtag(id: any, value: any) {
-        window.dataLayer.push([id, value]);
-      }
-      gtag('js', new Date());
-      gtag('config', this.googleTagId);
+      (function (w: any, d: any, s: any, l: any, i: any) {
+        w[l] = w[l] || [];
+        w[l].push({
+          'gtm.start': new Date().getTime(),
+          event: 'gtm.js',
+        });
+        var f = d.getElementsByTagName(s)[0],
+          j = d.createElement(s),
+          dl = l !== 'dataLayer' ? '&l=' + l : '';
+        j.async = true;
+        j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
+        f.parentNode.insertBefore(j, f);
+      })(window, document, 'script', 'dataLayer', this.googleTagId);
     }
   }
 
